7e54aabde8b3111dad1af0353efa9bfad2bdd2f8
Archie Mourad <013933@asas.qld.edu.au>
2021-12-24 12:42:49 +1000

    added flashing text bar

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,9 +13,12 @@ function App() {
     <body>
       <div className='bg'></div>
         <div className='nav-button--selected'><Link to='/Nexus'>Home</Link></div><div className='nav-button--notselected'><Link to='/Nexus/Browse'>Browse</Link></div>
-            <div className='heading'>Nexus</div>
+          <div className='heading--wrapper'>
+            <div className='heading'>Nexus</div><div className='heading--type'></div>
+          </div>
             <div className='heading-desc'>An index of (cheat)hacked clients, for any game.</div>
             <div className='browse-link'><Link to='/Nexus/Browse'>Start browsing supported titles ❯</Link></div>
+      <div className='footer'></div>
     </body>
   );
 }
